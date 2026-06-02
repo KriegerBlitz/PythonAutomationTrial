3a9f54fe5783fd5914823eda8c41bbfa24918af4
Krieger <blitzcartultimate@gmail.com>
2026-06-03 01:29:56 +0400

    Hmm

--- main.py
+++ main.py
@@ -8,27 +8,35 @@ stack = []
 loc = 'dummy_data'
 os.makedirs('result', exist_ok=True)
 
-log = []
+log = {}
+seen = []
 
-stack.append((loc, False, False))
+stack.append((loc, False))
 
 while stack:
-    loc, isOCT, isFUNDUS = stack.pop()
+    loc, isOCT = stack.pop()
     try:
         for sub in os.scandir(loc):
             if sub.is_dir(): #Probably shouldn't have called the path var dir
-                stack.append((sub.path, isOCT or sub.name == "OCT", isFUNDUS or sub.name == "FUNDUS"))
+                stack.append((sub.path, isOCT or sub.name == "OCT"))
             elif sub.is_file():
-                if not isOCT or (sub.name == "016.png" and not isFUNDUS):
-                    if not os.path.exists(os.path.join('result', sub.name)):
+                if not isOCT or (sub.name == "016.png"):
+
+                    if not os.path.exists(os.path.join('result', sub.name)): #Looking back probably should have made this a function
                         shutil.copy2(sub.path, os.path.join('result', sub.name))
+                        log[sub.name] = {'new name':sub.name, 'path':sub.path, 'old name':sub.name}
                     else:
-                        rename = os.path.abspath(sub.path).replace(os.path.sep, '_')
+                        rename = str(os.path.abspath(sub.path).replace(os.path.sep, '_'))
                         shutil.copy2(sub.path, os.path.join('result', rename)) #Why is this yellow
+                        log[rename] = {'new name': rename, 'path': sub.path, 'old name': sub.name} #Wait, I gotta rename the old one too?
+                        dic = log.pop(sub.name)
+                        rename = str(os.path.abspath(dic['path']).replace(os.path.sep, '_'))
+                        os.rename(os.path.join('result', sub.name), os.path.join('result', rename))
+                        log[rename] = {'new name': rename, 'path': dic['path'], 'old name': sub.name}
+
                 elif isOCT:
-                    pass
-                elif isFUNDUS:
-                    pass
+                    continue
+
 
 
 
