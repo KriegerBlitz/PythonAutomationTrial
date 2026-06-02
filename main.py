d94a2d3074dd38ae759c7c93c1eb7e76514430c7
Krieger <blitzcartultimate@gmail.com>
2026-06-03 00:53:17 +0400

    Start

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
 import os
+import shutil
 import zipfile
 import pandas as pd
 
 stack = []
 
-dir = 'dummy_data'
+loc = 'dummy_data'
 os.makedirs('result', exist_ok=True)
 
 log = []
 
-stack.append((dir, False, False))
+stack.append((loc, False, False))
 
 while stack:
-    dir, isOCT, isFUNDUS = stack.pop()
+    loc, isOCT, isFUNDUS = stack.pop()
     try:
-        for sub in os.scandir(dir):
+        for sub in os.scandir(loc):
             if sub.is_dir(): #Probably shouldn't have called the path var dir
                 stack.append((sub.path, isOCT or sub.name == "OCT", isFUNDUS or sub.name == "FUNDUS"))
             elif sub.is_file():
                 if not isOCT or (sub.name == "016.png" and not isFUNDUS):
-
-                else
+                    if not os.path.exists(os.path.join('result', sub.name)):
+                        shutil.copy2(sub.path, os.path.join('result', sub.name))
+                    else:
+                        rename = os.path.abspath(sub.path).replace(os.path.sep, '_')
+                        shutil.copy2(sub.path, os.path.join('result', rename)) #Why is this yellow
+                elif isOCT:
+                    pass
+                elif isFUNDUS:
+                    pass
 
 
 
