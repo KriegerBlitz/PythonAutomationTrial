166713100d02216f2478aa4c9a9d0e1982920f0c
Krieger <blitzcartultimate@gmail.com>
2026-06-06 13:37:34 +0400

    FUNDUS and polymorph

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import zipfile
 import csv
+from os import DirEntry
 
 stack = []
 
@@ -9,7 +10,22 @@ loc = 'dummy_data'
 os.makedirs('result', exist_ok=True)
 
 log = {}
-seen = []
+def copy(sub: os.DirEntry):
+    if not sub.name in log:
+        shutil.copy2(sub.path, os.path.join('result', sub.name))
+        log[sub.name] = {'new name': sub.name, 'path': sub.path, 'old name': sub.name, 'kill': False}
+    else:
+        rename = str(os.path.abspath(sub.path).replace(os.path.sep, '_'))
+        shutil.copy2(sub.path, os.path.join('result', rename))  # Why is this yellow
+        log[rename] = {'new name': rename, 'path': sub.path, 'old name': sub.name, 'kill': False}
+        log[sub.name]['kill'] = True
+        # dic = log.pop(sub.name)
+        # rename = str(os.path.abspath(dic['path']).replace(os.path.sep, '_'))
+        # os.rename(os.path.join('result', sub.name), os.path.join('result', rename))
+        # log[rename] = {'new name': rename, 'path': dic['path'], 'old name': sub.name}
+
+def checkimg(name:str):
+    return name.endswith(('.png', '.jpg', '.jpeg','.tif','tiff','.webp','.arw','.bmp'))
 
 stack.append((loc, False))
 
@@ -18,27 +34,20 @@ while stack:
     try:
         for sub in os.scandir(loc):
             if sub.is_dir(): #Probably shouldn't have called the path var dir
-                stack.append((sub.path, isOCT or sub.name == "OCT"))
-            elif sub.is_file():
-                if not isOCT or (sub.name == "016.png"):
+                if sub.name!= 'FUNDUS': stack.append((sub.path, isOCT or sub.name == "OCT"))
+                else:
+                    for fun in os.scandir(sub.path):
+                        if fun.name.startswith('0'):
+                            continue
+                        elif checkimg(fun.name):
+                            copy(fun)
 
-                    if not sub.name in log: #Looking back probably should have made this a function
-                        shutil.copy2(sub.path, os.path.join('result', sub.name))
-                        log[sub.name] = {'new name':sub.name, 'path':sub.path, 'old name':sub.name, 'kill':False}
-                    else:
-                        rename = str(os.path.abspath(sub.path).replace(os.path.sep, '_'))
-                        shutil.copy2(sub.path, os.path.join('result', rename)) #Why is this yellow
-                        log[rename] = {'new name': rename, 'path': sub.path, 'old name': sub.name, 'kill':False}
-                        log[sub.name]['kill'] = True
-                        # dic = log.pop(sub.name)
-                        # rename = str(os.path.abspath(dic['path']).replace(os.path.sep, '_'))
-                        # os.rename(os.path.join('result', sub.name), os.path.join('result', rename))
-                        # log[rename] = {'new name': rename, 'path': dic['path'], 'old name': sub.name}
+            elif sub.is_file() and checkimg(sub.name):
+                if not isOCT or (sub.name == "016.png"):
+                    copy(sub)
 
                 elif isOCT:
                     continue
-
-
 
 
     except PermissionError:
